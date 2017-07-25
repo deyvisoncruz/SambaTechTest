c6b93fad19a5351825ed0c3897a18ad07c02702e
deyvisoncasa <deyvison_cruz@yahoo.com.br>
2017-07-25 15:55:25 -0300

    Estrutura  - Configurando o Hibernate e suas classes

--- SambaTechTest/SambaTechTest/src/main/java/Util/HibernateUtil.java
+++ SambaTechTest/SambaTechTest/src/main/java/Util/HibernateUtil.java
@@ -5,35 +5,46 @@
  */
 package Util;
 
+
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.cfg.*;
-import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 /**
  *
  * @author Deyvison
  */
 
 
-
 public class HibernateUtil {
-
-    private static final SessionFactory sessionFactory;
     
-    static {
+    private static final SessionFactory sessionFactory;
+    public static final String HIBERNATE_SESSION = "hibernate_session";
+    
+    static{
+        
         try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            
+            System.out.println("Tentando configurar a SF");
+            
+            Configuration configuration = new Configuration().configure();
+            
+            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().
+                applySettings(configuration.getProperties()).buildServiceRegistry();
+            
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            
+            System.out.println("Session factory criada corretamente");
+        } catch (Exception ex) {
+            System.out.println("Ocorreu um  erro ao iniciar a SF. " + ex);
             throw new ExceptionInInitializerError(ex);
         }
+        
+        
     }
-    
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+    
 }
-
